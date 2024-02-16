b94eec4ed7cb4fd59aa4e614c5359270249f43e4
Kevin G <kevinfgoyesa@gmail.com>
2024-02-16 19:13:15 +1100

    Nuevos productos pagina basica

--- main.js
+++ main.js
@@ -9,3 +9,29 @@ function toggleDesktopMenu() {
 }
 
 console.log('JS funcionando');
+
+
+const productlist = [];
+
+productlist.push ({
+    name: "bike",
+    precio: "100",
+    imagen: "hp1"
+});
+
+productlist.push ({
+    name: "tv",
+    precio: "200",
+    imagen: "hp2"
+});
+
+productlist.push ({
+    name: "cel",
+    precio: "300",
+    imagen: "hp3"
+});
+
+
+for (const producto of productlist) {
+    console.log(producto.name);
+}
